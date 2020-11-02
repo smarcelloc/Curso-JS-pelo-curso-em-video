abb24b2d668ced00e66828315170e8bfbd3a0530
Marcello Silvério <smarcelloc@gmail.com>
2020-11-02 20:46:27 -0300

    Implementado uma calculadora com histórico

---
+++ src/dom/introducao.js
@@ -0,0 +1,4 @@
+console.log("Document Object Model\n");
+console.log("é uma interface independente de plataforma cruzada e de linguagem que trata",
+"um documento XML ou HTML como uma estrutura de árvore em que cada nó é um objeto que", 
+"representa uma parte do documento.");
